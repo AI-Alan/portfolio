c40a1d92e6403880641df705b0a37d9199579d67
Aman kumar yadav <amorphiouslattice@gmail.com>
2025-02-09 20:38:52 +0530

    Fix GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 
 
 import Home from "./components/home";
